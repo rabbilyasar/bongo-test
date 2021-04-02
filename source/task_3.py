9e475c0aa4c9c747d8e0e1630812dce43a13ffbf
rabbilyasar <rabbilyasar@gmail.com>
2021-04-02 20:46:57 +0600

    add task_3

---
+++ source/task_3.py
@@ -0,0 +1,26 @@
+class TreeNode:
+    def __init__(self, value, parent) -> None:
+        self.value = value
+        self.parent = parent
+
+def lca(node1, node2):
+    if node1.value == node2.value:
+        print(node1.value)
+        return
+
+    node1_values = [node1.value]
+    node2_values = [node2.value]
+
+    while True:
+        if node1.parent:
+            node1 = node1.parent
+            if node1.value in node2_values:
+                print(node1.value)
+            node1_values.append(node1.value)
+
+        if node2.parent:
+            node2 = node2.parent
+            if node2.value in node1_values:
+                print(node2.value)
+                return
+            node2_values.append(node2.value)
